a18108195370aee29d217c47deadd34459dd5b81
0xb0rn3 <154826956+0xb0rn3@users.noreply.github.com>
2025-05-24 04:42:53 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"context"
 	"encoding/json"
 	"encoding/xml"
@@ -12,8 +11,8 @@ import (
 	"net"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
-	"sync"
 	"time"
 
 	"github.com/google/gopacket"
